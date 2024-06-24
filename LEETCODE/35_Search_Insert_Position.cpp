555669b04d9617dad2d14d0feeb6e575e45a2ea6
DhyanKamal <kdhyan94@gmail.com>
2024-06-25 01:07:22 +0530

    added new data in GFG

---
+++ LEETCODE/35_Search_Insert_Position.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int mid,start=0,end=nums.size()-1;
+        while(start<=end){
+            mid=start+ (end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else 
+            start=mid+1;
+        
+        }
+        return start;
+    }
+};
